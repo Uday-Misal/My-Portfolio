ccabce4401a6c09c95f83fdb8595e926743961ec
Uday <udaymisal488@gmail.com>
2025-12-12 15:39:15 +0530

    changes in project

--- my-portfolio/src/components/Project.jsx
+++ my-portfolio/src/components/Project.jsx
@@ -22,28 +22,22 @@ const Projects = () => {
 
   const projects = [
     {
-      title: "Gaming E-Commerce UI",
-      desc: "A modern, responsive interface for a gaming gear store built with React and CSS.",
-      tech: ["React", "CSS", "JS"],
-      link: "#",
+      title: "Smart-ATS-Resume-and-Portfolio-Builder",
+      desc: "A web application that helps users create ATS-friendly resumes and portfolios with customizable templates and real-time previews.",
+      tech: ["React", "CSS", "Node.js"],
+      link: "https://github.com/Uday-Misal/Smart-ATS-Resume-and-Portfolio-Builder",
     },
     {
-      title: "Chat App",
-      desc: "Real-time chat application built with Node.js and Socket.io.",
-      tech: ["Node.js", "Socket.io", "Express"],
-      link: "#",
-    },
-    {
-      title: "Portfolio Website",
-      desc: "Personal portfolio built with React showcasing projects and experience.",
-      tech: ["React", "HTML", "CSS"],
-      link: "#",
+      title: "SportsAssissmentApp",
+      desc: "An application designed to assess and track athletic performance through various tests and metrics.",
+      tech: ["Node.js", "React", "MySql"],
+      link: "https://github.com/Uday-Misal/SportsApp",
     },
     {
       title: "Portfolio Website",
       desc: "Personal portfolio built with React showcasing projects and experience.",
-      tech: ["React", "HTML", "CSS"],
-      link: "#",
+      tech: ["React"],
+      link: "https://github.com/Uday-Misal/My-Portfolio",
     },
   ];
 
